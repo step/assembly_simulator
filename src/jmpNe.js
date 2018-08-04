2e89172f019d7491fa47fb682856c0987b88d86e
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-08-04 14:46:22 +0530

    Added instruction to jump if not equal

---
+++ src/jmpNe.js
@@ -0,0 +1,16 @@
+class JmpNe {
+  constructor(lineNumber) {
+    this.lineNumber=lineNumber;
+  }
+
+  execute(regs,flags) {
+    let {NE} = flags;
+    let nextLine=undefined;
+    if(NE == 1) {
+      nextLine = this.lineNumber;
+    }
+    return {regs,flags,nextLine};
+  }
+}
+
+module.exports = JmpNe;
