d8c3a6e6ef4fa5023a7f6f745513d1f5d35d08d2
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-08-04 14:15:00 +0530

    Added instruction to compare between a register and a value and set flags

---
+++ src/cmpRegToVal.js
@@ -0,0 +1,24 @@
+class CmpRegToVal {
+  constructor(lhs,rhs) {
+    this.lhs=lhs;
+    this.rhs=rhs;
+  }
+
+  execute(currRegs,currFlags) {
+    let newFlags={EQ:0,NE:0,GT:0,LT:0};
+    let val=currRegs[this.lhs];
+    if(val==this.rhs) {
+      newFlags["EQ"] = 1;
+    } else {
+      newFlags["NE"] = 1;
+      if (val>this.rhs) {
+        newFlags["GT"] = 1;
+      } else {
+        newFlags["LT"] = 1;
+      }
+    }
+    return {regs:currRegs,flags:newFlags};
+  }
+}
+
+module.exports = CmpRegToVal;
