3a00228b17400d520b785ec1758b76335ac2b0bf
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-08-04 14:30:08 +0530

    More tests around comparison between two registers

--- test/testCmpRegToReg.js
+++ test/testCmpRegToReg.js
@@ -11,11 +11,19 @@ describe("CmpRegToReg execution",function(){
     assert.deepEqual({A:10,B:10,C:0,D:0},regs);
   });
   it("should compare two unequal values and set the NE flag",function(){
-    let cmp=new CmpRegToVal("A",11);
-    let currRegs={A:10,B:0,C:0,D:0};
+    let cmp=new CmpRegToReg("A","B");
+    let currRegs={A:10,B:11,C:0,D:0};
     let currFlags={EQ:0,NE:0,GT:0,LT:0};
     let {regs,flags} = cmp.execute(currRegs,currFlags);
     assert.deepEqual({EQ:0,NE:1,GT:0,LT:1},flags);
-    assert.deepEqual({A:10,B:0,C:0,D:0},regs);
+    assert.deepEqual({A:10,B:11,C:0,D:0},regs);
+  });
+  it("should compare a greater value and set the GT and NE flag",function(){
+    let cmp=new CmpRegToReg("A","B");
+    let currRegs={A:10,B:9,C:0,D:0};
+    let currFlags={EQ:0,NE:0,GT:0,LT:0};
+    let {regs,flags} = cmp.execute(currRegs,currFlags);
+    assert.deepEqual({EQ:0,NE:1,GT:1,LT:0},flags);
+    assert.deepEqual({A:10,B:9,C:0,D:0},regs);
   });
 });
