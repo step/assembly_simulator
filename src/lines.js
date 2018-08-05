3a1e228f1f8ee52e0c02b59169c51af1f9122a8a
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-08-05 19:56:28 +0530

    Refactored lines' execute code and simplified it

--- src/lines.js
+++ src/lines.js
@@ -10,27 +10,24 @@ class Lines {
     this.lineNumbers[line.getLineNumber()] = index;
   }
 
+  shouldExecute(pc, halt) {
+    return pc < this.lines.length && !halt;
+  }
+
   execute(initState, cb) {
-    let currRegs = initState.regs;
-    let currFlags = initState.flags;
-    let currentIndex = 0;
-    let shouldHalt = false;
-    while (currentIndex < this.lines.length && !shouldHalt) {
-      let line = this.lines[currentIndex];
-      let { regs, flags, nextLine, currLine, halt, prn } = line.execute(
-        currRegs,
-        currFlags
-      );
-      currRegs = regs;
-      currFlags = flags;
-      currentIndex++;
-      shouldHalt = halt;
-      if (nextLine) currentIndex = this.lineNumbers[nextLine];
+    let { regs, flags } = initState;
+    let state = { regs, flags, halt: false };
+    let programCounter = 0;
+    while (this.shouldExecute(programCounter, state.halt)) {
+      let line = this.lines[programCounter];
+      state = line.execute(state.regs, state.flags);
+      programCounter++;
+      if (state.nextLine) programCounter = this.lineNumbers[state.nextLine];
       else {
-        let lineToExecute = this.lines[currentIndex];
-        nextLine = lineToExecute ? lineToExecute.getLineNumber() : 'STOP';
+        let lineToExecute = this.lines[programCounter];
+        state.nextLine = lineToExecute ? lineToExecute.getLineNumber() : ' ';
       }
-      cb({ regs, flags, nextLine, currLine, prn });
+      cb(state);
     }
   }
 }
