8e9da3a390f0abcd1c7d51cc565495c0ea36321e
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-08-07 10:58:55 +0530

    Adding CircleCI configuration and removing run.js

--- run.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const Machine = require("./src/machine.js");
-
-let fooAsm=fs.readFileSync("./foo.asm","utf8");
-
-let machine = new Machine();
-machine.load(fooAsm);
-machine.execute();
-console.log(machine.getPrn().join("\n"));
