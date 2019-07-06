b9317c684eae4a3cc65bf8cb53ee7b436b67533c
Ankoncha <44019225+luciferankon@users.noreply.github.com>
2019-07-06 17:25:30 +0530

    set name for MaxInstructionsExceededException

--- src/maxInstructionsExceededException.js
+++ src/maxInstructionsExceededException.js
@@ -1,6 +1,7 @@
 class MaxInstructionsExceededException extends Error {
   constructor(maxLinesToExecute) {
     super();
+    this.name = this.constructor.name;
     this.maxLinesToExecute = maxLinesToExecute;
   }
 }
